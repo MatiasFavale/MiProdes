cb841bb5b20e34b33388ba5b720aa25c1c0907db
favalem <matias.favale@molinos.com.ar>
2022-08-31 00:55:24 -0300

    commit message

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const store = configureStore();
 
 render(
   <ReduxProvider  store={store}>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </ReduxProvider>,
